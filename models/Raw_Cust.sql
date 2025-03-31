d34957d6f115861af1b7a996db842fdc2cb970d1
Rakesh_repo <63310664+rakeshsingh21@users.noreply.github.com>
2025-03-31 17:37:57 -0400

    Update Raw_Cust.sql

--- models/Raw_Cust.sql
+++ models/Raw_Cust.sql
@@ -1,7 +1,7 @@
 SELECT * , {{ get_ts_usr_acname() }}
 from 
-    DA_HELIX_DB_TEST.HELIX_RAW."raw_customer" C 
+    DA_HELIX_DB_TEST.HELIX_STG.RAW_CUSTOMER C 
 WHERE 
-    EXISTS (select n_name from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.NATION A where A.n_name = C."c_nation_name" )
-AND EXISTS (select r_name from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.REGION B where B.r_name = C."c_region_name")    
-limit 200 
+    EXISTS (select n_name from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.NATION A where A.n_name = C.c_nation_name )
+AND EXISTS (select r_name from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.REGION B where B.r_name = C.c_region_name)    
+limit 210
