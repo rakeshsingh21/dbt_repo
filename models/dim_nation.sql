3f0d14e702a4c417ea75805ad4c0dbfe2bf3a8d5
Rakesh_repo <63310664+rakeshsingh21@users.noreply.github.com>
2025-04-02 01:10:39 -0400

    Create dim_nation.sql

---
+++ models/dim_nation.sql
@@ -0,0 +1,14 @@
+{{
+    config( 
+        target_database='DA_HELIX_DB_TEST',
+        target_schema='HELIX_CDW',
+        unique_key='n_nationkey' 
+    )
+}}
+
+SELECT B.n_nationkey as n_nationkey, B.n_regionkey as n_regionkey , A.c_nation_name as c_nation_name, A.c_nation_comment as c_nation_comment, 
+       {{ get_ts_usr_acname() }} 
+FROM 
+    {{ ref('stg_customer')}} A
+INNER JOIN SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.nation B where B.n_name = A.c_nation_name
+qualify row_number() over(partition by n_nationkey order by n_nationkey) =1 
